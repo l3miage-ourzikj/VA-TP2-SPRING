48c2f04be7fd58fff2effba881ea0c5fe313af1f
l3miage-ourzikj <Jugurta.Ourzik@etu.univ-grenoble-alpes.fr>
2024-03-31 23:44:51 +0200

    +main: GetPlaylistTest overall done in PlaylistServiceTest

--- server/src/test/java/fr/uga/l3miage/spring/tp2/exo1/services/PlaylistServiceTest.java
+++ server/src/test/java/fr/uga/l3miage/spring/tp2/exo1/services/PlaylistServiceTest.java
@@ -5,8 +5,11 @@ import fr.uga.l3miage.exo1.requests.PlaylistCreationRequest;
 import fr.uga.l3miage.exo1.response.PlaylistResponseDTO;
 import fr.uga.l3miage.spring.tp2.exo1.components.PlaylistComponent;
 import fr.uga.l3miage.spring.tp2.exo1.components.SongComponent;
+import fr.uga.l3miage.spring.tp2.exo1.exceptions.rest.NotFoundEntityRestException;
+import fr.uga.l3miage.spring.tp2.exo1.exceptions.technical.NotFoundPlaylistEntityException;
 import fr.uga.l3miage.spring.tp2.exo1.mappers.PlaylistMapper;
 import fr.uga.l3miage.spring.tp2.exo1.models.PlaylistEntity;
+import fr.uga.l3miage.spring.tp2.exo1.models.SongEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -14,9 +17,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.DURATION;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.Mockito.*;
 
@@ -46,8 +52,9 @@ public class PlaylistServiceTest {
 
         PlaylistEntity playlistEntity = playlistMapper.toEntity(playlistCreationRequest);
         playlistEntity.setSongEntities(Set.of());
+
         when(songComponent.getSetSongEntity(same(Set.of()))).thenReturn(Set.of());
-        when(playlistComponent.createPlaylistEntity(playlistEntity)).thenReturn(playlistEntity);
+        when(playlistComponent.createPlaylistEntity(any(PlaylistEntity.class))).thenReturn(playlistEntity);
 
         PlaylistResponseDTO responseExpected = playlistMapper.toResponse(playlistEntity);
 
@@ -57,8 +64,80 @@ public class PlaylistServiceTest {
         // then
         assertThat(response).usingRecursiveComparison().isEqualTo(responseExpected);
         verify(playlistMapper,times(2)).toEntity(playlistCreationRequest);
-        verify(playlistMapper,times(1)).toResponse(same(playlistEntity));
+        verify(playlistMapper,times(2)).toResponse(same(playlistEntity));
         verify(playlistComponent,times(1)).createPlaylistEntity(any(PlaylistEntity.class));
         verify(songComponent,times(1)).getSetSongEntity(Set.of());
     }
+    @Test
+    void getPlaylistFound() throws NotFoundPlaylistEntityException {
+        //given
+        PlaylistEntity playlist= PlaylistEntity.builder().name("HitList").songEntities(Set.of()).build();
+
+        //when
+        when(playlistComponent.getPlaylist(anyString())).thenReturn(playlist);
+
+        PlaylistResponseDTO expectedResponse= playlistMapper.toResponse(playlist);
+        PlaylistResponseDTO response =playlistService.getPlaylist("HitList");
+
+        //then
+        assertThat(response).usingRecursiveComparison().isEqualTo(expectedResponse);
+        verify(playlistMapper, times(2)).toResponse(same(playlist));
+        verify(playlistComponent, times(1)).getPlaylist(anyString());
+
+
+
+
+
+    }
+    @Test
+    void getPlaylistNotFound() throws NotFoundPlaylistEntityException {
+        //when
+        when(playlistComponent.getPlaylist(anyString())).thenThrow(new NotFoundPlaylistEntityException("Playlist not found"));
+
+    //then
+        assertThrows(NotFoundEntityRestException.class,()->playlistService.getPlaylist("Hit"));
+
+
+
+
+
+    }
+
+    @Test
+    void  addSongInPlaylistPossible() throws NotFoundPlaylistEntityException {
+        //given
+        PlaylistEntity playlistEntity = PlaylistEntity.builder().name("HitList").songEntities(Set.of()).build();
+        SongEntity son= SongEntity.builder().title("felecita").build();
+        playlistEntity.getSongEntities().add(son);
+        playlistComponent.createPlaylistEntity(playlistEntity);
+
+
+        //when
+        when(songComponent.getSetSongEntity(anySet())).thenReturn(Set.of(son));
+        when(playlistComponent.addSong(anyString(),any(SongEntity.class))).thenReturn(playlistEntity);
+
+        PlaylistResponseDTO expectedResponse = playlistMapper.toResponse(playlistEntity);
+
+        PlaylistResponseDTO response=playlistService.addSongInPlaylist("HitList","felecita");
+
+        assertThat(response).usingRecursiveComparison().isEqualTo(expectedResponse);
+        verify(playlistMapper,times(2)).toResponse(same(playlistEntity));
+        verify(playlistComponent,times(1)).addSong(anyString(),any(SongEntity.class));
+
+
+
+
+
+    }
+
+    //Quand ce n'est pas possible, c'est parce que la playlist ou le son n'est pas trouvé
+   /* @Test
+    void  addSongInPlaylistNotPossible(){
+
+    }
+
+    */
+
+
+
 }
